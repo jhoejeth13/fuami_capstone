69b61cba8e45129f90af3b7e46009cfec30fac56
jhoejeth <jhoejeth@sos.com>
2025-04-20 01:38:12 +0800

    added 200 data

---
+++ database/seeders/StudentAndGraduateSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Juniorhighschool;
+use App\Models\Graduate;
+use Faker\Factory as Faker;
+
+class StudentAndGraduateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        // Create 100 male and 100 female students
+        foreach (['Male', 'Female'] as $gender) {
+            for ($i = 0; $i < 100; $i++) {
+                Juniorhighschool::create([
+                    'lrn_number' => $faker->unique()->numerify('##########'),
+                    'first_name' => $faker->firstName($gender),
+                    'middle_name' => $faker->firstName,
+                    'last_name' => $faker->lastName,
+                    'suffix' => $faker->randomElement(['Jr.', 'Sr.', 'II', 'III', '']),
+                    'gender' => $gender,
+                    'birthdate' => $faker->date(),
+                    'address' => $faker->address,
+                    'school_year' => $faker->year,
+                    'photo_path' => null,
+                ]);
+            }
+        }
+
+        // Create 100 male and 100 female graduates
+        foreach (['Male', 'Female'] as $gender) {
+            for ($i = 0; $i < 100; $i++) {
+                Graduate::create([
+                    'ID_student' => $faker->unique()->numerify('##########'),
+                    'first_name' => $faker->firstName($gender),
+                    'middle_name' => $faker->firstName,
+                    'last_name' => $faker->lastName,
+                    'gender' => $gender,
+                    'strand' => $faker->randomElement(['STEM', 'ABM', 'HUMSS', 'TVL']),
+                    'year_graduated' => $faker->year,
+                ]);
+            }
+        }
+    }
+}
